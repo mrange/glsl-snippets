add7a98fdf5fbf45534fbdf91dd211a28c1f228a
mrange <marten_range@hotmail.com>
2024-02-03 22:31:09 +0100

    oklab

--- src/snippets.glsl
+++ src/snippets.glsl
@@ -801,3 +801,26 @@ vec4 fxaa(sampler2D tex, vec2 uv, vec2 texelSz) {
   //If the average is outside the luma range, using the middle average
   return ((lumaB < lumaMin) || (lumaB > lumaMax)) ? A : B;
 }
+
+// License: MIT, author: Inigo Quilez, found: https://www.shadertoy.com/view/ttcyRS
+vec3 oklab_mix(vec3 lin1, vec3 lin2, float a) {
+    // https://bottosson.github.io/posts/oklab
+    const mat3 kCONEtoLMS = mat3(
+         0.4121656120,  0.2118591070,  0.0883097947,
+         0.5362752080,  0.6807189584,  0.2818474174,
+         0.0514575653,  0.1074065790,  0.6302613616);
+    const mat3 kLMStoCONE = mat3(
+         4.0767245293, -1.2681437731, -0.0041119885,
+        -3.3072168827,  2.6093323231, -0.7034763098,
+         0.2307590544, -0.3411344290,  1.7068625689);
+
+    // rgb to cone (arg of pow can't be negative)
+    vec3 lms1 = pow( kCONEtoLMS*lin1, vec3(1.0/3.0) );
+    vec3 lms2 = pow( kCONEtoLMS*lin2, vec3(1.0/3.0) );
+    // lerp
+    vec3 lms = mix( lms1, lms2, a );
+    // gain in the middle (no oklab anymore, but looks better?)
+    lms *= 1.0+0.2*a*(1.0-a);
+    // cone to rgb
+    return kLMStoCONE*(lms*lms*lms);
+}
